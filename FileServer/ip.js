4acbe5379a1bca11a87df837c6b1f7ebfba873dc
Mikael Kindborg <mikael.kindborg@gmail.com>
2013-07-09 22:52:07 +0200

    Experimenting with better way of getting server ip address, and started looking at node-webkit

--- FileServer/ip.js
+++ FileServer/ip.js
@@ -1,7 +1,22 @@
-require('dns').lookup(require('os').hostname(),
+/*require('dns').lookup(require('os').hostname(),
   function (err, add, fam)
   {
     console.log('err: ' + err)
     console.log('add: ' + add)
     console.log('fam: ' + fam)
   })
+*/
+var os = require('os');
+var interfaces = os.networkInterfaces();
+for(name in interfaces) {
+ console.log(name)
+ var interface = interfaces[name];
+ interface.forEach(function(entry) {
+   console.log("  " + entry.address)
+   if(entry.family === "IPv4") {
+     //var s = net.createServer(connectionHandler);
+     //s.listen(80, entry.address);
+     console.log("  listening on " + entry.address);
+   }
+ });
+}
